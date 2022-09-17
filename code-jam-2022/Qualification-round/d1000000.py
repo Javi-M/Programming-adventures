a95fc472207473c79b38ea071011ec8e7941a5f0
Javier Marquez <javier0marquezr@gmail.com>
2022-09-17 13:20:54 +0200

    solve: d1000000
    code-jam-2022/Qualification-round/d1000000 -> Tests passed

--- code-jam-2022/Qualification-round/d1000000.py
+++ code-jam-2022/Qualification-round/d1000000.py
@@ -1,39 +1,27 @@
 # Third problem from the qualifiaciton round
-
 # A d1000000 is a dice with 1000000 sides.
-# Collection of N dice.
-
-# Input:
-# T - test cases
-# for each test case, two lines:
-# N - number of dice
-# Integers - (representing, each one, the nº of sides of its dice)
-
-# Output: the maximum number of input dice that can be put in a straight
-# (check the conditions from the original statement)
 
 T = int(input())
 total_solution = [] # This will be the list of solutions (each one a list)
 
-def solve_case(dices):
+# Main algorithm:
+def solve_case(N:int, dices:list):
     ml = 0 # max. len. found
     dices.sort()
-    n = 0
-    d = 0 # displacement
-    for i in range(0, dices):
-        n = i + 1 - d
-        if dices[i] >= n:
+    for i in range(0, N):
+        if dices[i] > ml:
             ml = ml + 1
-        else:
-            d = d + 1
     return ml
 
+# Solving all the cases:
 for case in range(0, T):
-    N = input()
+    N = int(input())
     line = input().split()
     dices = [int(n) for n in line] # List comprehension!
-    total_solution.append(solve_case(dices))
+    total_solution.append(solve_case(N, dices))
 
+# Printing all the cases:
+total_solution = [str(c) for c in total_solution] # converting int to str
 for case in range(0, T):
-    print('Case #' + str(case+1) + ':' + total_solution[case])
+    print('Case #' + str(case+1) + ': ' + total_solution[case])
    
