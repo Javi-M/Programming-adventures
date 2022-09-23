a3ad9dda218b8b54273e2fac00e453091085d0eb
Javier Marquez <javier0marquezr@gmail.com>
2022-09-23 15:51:38 +0200

    add file & enhance README
    Equal-Sum added. Not solved. README now has badges of the lenguages

---
+++ code-jam-2022/Round-1A/Equal-Sum.py
@@ -0,0 +1,13 @@
+# INTERACTIVE PROBLEM
+# 3 PHASES
+## 1. Choose N distinct integers
+## 2. 
+
+
+T = int(input())
+
+
+
+for case in range(0, T):
+    N = int(input())
+    # integers = list(map(int, input().split()))
