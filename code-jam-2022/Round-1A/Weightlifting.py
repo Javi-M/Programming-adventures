8deb9637c782992b9760566c8f57b33328637a62
Javier Marquez <javier0marquezr@gmail.com>
2022-09-26 12:01:52 +0200

    add(n_ops, function): Weightlifting
    The distance function n_ops(stack1, stack2) has been written. Not tested yet.

--- code-jam-2022/Round-1A/Weightlifting.py
+++ code-jam-2022/Round-1A/Weightlifting.py
@@ -8,16 +8,40 @@ T = int(input())
 
 total_solution = []
 
+##
+# EXERCISES MUST BE DONE IN ORDER
+# Def. distance: n_ops(stack1, stack2)
+# Representations of stacks, 2 examples:
+# [1, 1, 1, 2, 2, 2]
+# [1, 2, 1, 2, 1, 2]
+# would mean: 3 discs type 1, 3 discs type 2.
+def n_ops(s1, s2):
+    n = 0
+    i = 0
+    while (i < len(s1) and s1[i]==s2[i]):
+        i = i+1
+    if i < range(len(s1)): # i.e: they are not the same stack
+        n = (len(s1) - i)*2
+    return n
+
 for case in range(T):
-    E, W = list(map(int, input().split())) # We suppose correct input
+    E, W = list(map(int, input().split())) # We suppose correct input.
     
-    # now it comes E lines
-    # each line is an exercises, with the weights required
+    # Now it comes E lines.
+    # Each line is an exercises, with the weights required.
+    # [4, 5, 6] -> 4 weights of type 1. 6 weights of type 3. 5w type 2.
+
+    # Exercises is a matrix. Each row contains the weights for an exercies.
     exercises = []
-    machine_stack = []
+    weights = []
+    # machine_stack = []
     for i in range(E):
+        weights = list(map(int, input().split()))
+        exercises.append(weights)
         pass
 
+for r in exercises:
+    print(r)
 
-for case in range(T):
-    print('Case #' + str(case+1) + total_solution[case])
+# for case in range(T):
+#     print('Case #' + str(case+1) + total_solution[case])
