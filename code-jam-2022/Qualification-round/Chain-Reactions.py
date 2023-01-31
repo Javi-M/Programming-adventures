606698e4e2d3fe72a8ac461c0a69c955e42a810a
Javier Marquez <javier0marquezr@gmail.com>
2023-01-31 22:40:01 +0100

    nothing new
    Chain-Reactions still does not work completely

--- code-jam-2022/Qualification-round/Chain-Reactions.py
+++ code-jam-2022/Qualification-round/Chain-Reactions.py
@@ -2,38 +2,52 @@ from itertools import permutations
 
 # To solve "indexing confusion": right shift to input lists: F and P
 # Also right shift triggered
-def trigger(idx, triggered, F, P):
-    max_fun = F[idx]
+def trigger(idx, triggered, F2, P2):
+    # THIS VERSION EXCEEDS MEMORY USAGE!
+    # max_fun = F[idx]
+    # if triggered[idx]:
+    #     return 0
+    # triggered[idx] = True
+    # if P[idx] == 0:
+    #     return F[idx]
+    # else:
+    #     idx2 = P[idx]
+    #     f2 = trigger(idx2, triggered, F, P)
+    #     max_fun = max(max_fun, f2)
 
-    if triggered[idx]:
-        return 0
+    max_fun_chain = F2[idx]
 
-    triggered[idx] = True
+    there_is_next = not triggered[P2[idx]] and P2[idx] != 0
 
-    if P[idx] == 0:
-        return F[idx]
-    else:
-        idx2 = P[idx]
-        f2 = trigger(idx2, triggered, F, P)
-        max_fun = max(max_fun, f2)
+    while there_is_next:
+        f2 = F2[P2[idx]]
+        max_fun_chain = max(f2, max_fun_chain)
+        triggered[P2[idx]] = True
+        idx = P2[idx]
+        there_is_next = not triggered[P2[idx]] and P2[idx] != 0
 
-    return max_fun
+    return max_fun_chain
 
 
 
 def solve_case(N, F, P):
+    if N == 0:
+        return 0
+
     F2 = [0] + F     # RIGHT SHIFT
     P2 = [0] + P     # RIGHT SHIFT
 
-    inis = range(1, N+1)
+    inis = list(range(1, N+1))
     initiators = [x for x in inis if not x in P2] # Not pointed by anything
 
     all_possible_orders = list(permutations(initiators))
 
     max_fun = 0
-
+    chain_fun = 0
+    order_fun = 0
     for order in all_possible_orders:
         order_fun = 0
+        chain_fun = 0
         triggered = [True] + [False]*N # RIGHT SHIFT
         for i in order:
             chain_fun = trigger(i, triggered, F2, P2)
@@ -41,8 +55,12 @@ def solve_case(N, F, P):
         
         if order_fun > max_fun:
             max_fun = order_fun
-
+            
     return max_fun
+
+
+
+
 
 if __name__ == "__main__":
     solutions = []
