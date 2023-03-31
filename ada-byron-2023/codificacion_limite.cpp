1eab5b57a977be3bd1344aa45dc2b62451f22bd4
Javier Marquez <javier0marquezr@gmail.com>
2023-04-01 01:44:21 +0200

    improve codif.. lim
    Codificación límite (no el 2) ha sido ligeramente mejorado,
    cambiando 'cout' por 'printf'

--- ada-byron-2023/codificacion_limite.cpp
+++ ada-byron-2023/codificacion_limite.cpp
@@ -16,19 +16,28 @@ int main()
         int n =  line.length();
         for(int i = 0; i < n-2; i++){
             if(line[i] != '.' && line[i+1] == '.' && line[i+2] == '.'){
-                cout << line[i];
+                printf("%c", line[i]);
             }
         }
-        cout << '\n'; // '\n' mas rapido que endl
+        printf("%c", '\n'); // '\n' mas rapido que endl
     }
     return 0;
 }
 
 /* EXPLICACIÓN
+Por cada línea input, escribe una palabra.
 Cuando lee una linea, al encontrar una letra y 2 puntos, esa letra
 debe añadirse a la palabra. Al cambiar de línea de input,
 empezamos otra palabra.
-Como es apreciable, "line" es recorrida 2 veces:
+Por ejemplo:
+INPUT:
+a..b..c..dads.asd.f.d..e
+jklm..no.p.q..r.s..
+SALIDA:
+abcd
+mqs
+
+Como es apreciable en el código, "line" es recorrida 2 veces:
 para recibirla con getline y para encontrar las letras y puntos.
 Poder recorrer cada linea de input una vez, caracter a caracter,
 aumentaría la velocidad.
--- ada-byron-2023/codificacion_limite2.cpp
+++ ada-byron-2023/codificacion_limite2.cpp
@@ -2,21 +2,44 @@
 
 #include <bits/stdc++.h>
 #include <iostream>
+#include <istream>
 #include <stdio.h>
 #include <string> 
 
 using namespace std;
 
 
+// En c++, eof() devuelve End Of File
 int main() 
 {
     ios_base::sync_with_stdio(false); //Template para prog. competitiva
     cin.tie(NULL); // Template para prog. competitiva
 
-    char c;
+    char c_in; // Caracter leyendo de entrada
+    char c_w; // Caracter de la palabra a imprimir
 
-    scanf("%c", &c);
-    cout << c << '\n';
+    scanf("%c", &c_in);
+    while(c_in != EOF){
+        cin >> c_in;
+        if(c_in == '\n'){
+            cout << '\n';
+        }else if(c_in != '.'){
+            c_w = c_in;
+            scanf("%c", &c_in);
+            if(c_in == '.'){
+                scanf("%c", &c_in);
+                if(c_in == '.'){
+                    // Si llega aquí, ha encontrado
+                    // letra-punto-punto, y se añade la letra
+                    cout << c_w;
+                }else if(c_in == '\n'){
+                    cout << '\n';
+                }
+            }else if(c_in == '\n'){
+                cout << '\n';
+            }
+        }
+    }
 
     return 0;
 }
@@ -25,4 +48,6 @@ int main()
 Este es un segundo intento para aumentar la velocidad.
 Se intenta leer caracter a caracter en vez de por línea.
 
+El código queda horroroso, por motivos de la competición. Se recompensa
+el grano fino al dedillo, en vez de un buen algoritmo.
 */
